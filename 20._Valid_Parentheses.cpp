734021c4522e88925f168f46e2493b9086a0c407
zuzqqa <zuzannanowak04@wp.pl>
2023-11-04 15:11:33 +0100

    Update 20._Valid_Parentheses.cpp
    Given a string s containing just the characters '(', ')', '{', '}', '[' and ']', determine if the input string is valid.
    
    An input string is valid if:
    
    Open brackets must be closed by the same type of brackets.
    Open brackets must be closed in the correct order.
    Every close bracket has a corresponding open bracket of the same type.

--- 20._Valid_Parentheses.cpp
+++ 20._Valid_Parentheses.cpp
@@ -1,33 +1,15 @@
-bool isValid(string s) {
-    int b = 0;
-    
-  
-    if( s.length() % 2 == 1 )
-      return false;
-    
-    if ( s[0] == ')' || s[0] == '}' || s[0] == ']' ) 
-      return false;
+ bool isValid(std::string s) {
+     std::stack <char> st;
 
-    for( int i = 0; i < s.length() - 1; i++ ){
-      switch(s[i]){
-        case '{':
-          for( int j = i + 1; j < s.length(); j++ ){
-            if(s[j] == '}') b = 1;
-          }
-          if( b != 1 ) return false;
-          break;
-        case '[':
-          for( int j = i + 1; j < s.length(); j++ ){
-            if(s[j] == ']') b = 1;
-          }
-          break;
-        case '(':
-          for( int j = i + 1; j < s.length(); j++ ){
-            if(s[j] == ')') b = 1;
-          }
-          if( b != 1 ) return false;
-          break;
-      }
-    }
-  return true;
-} 
+     
+     for (char c : s) {
+         if (c == '(' || c == '[' || c == '{')
+             st.push(c);
+         else {
+             if (st.empty() || (c == ')' && st.top() != '(') || (c == ']' && st.top() != '[') || (c == '}' && st.top() != '{'))
+                 return false;
+            st.pop();
+         }
+     }
+     return st.empty();
+ }
