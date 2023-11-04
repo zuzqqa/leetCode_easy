8ac0bd97d26206c424cf11bdc02fe938c38db58b
zuzqqa <zuzannanowak04@wp.pl>
2023-11-04 17:26:44 +0100

    Create 101._Symmetric_Tree.cpp
    Given the root of a binary tree, check whether it is a mirror of itself (i.e., symmetric around its center).

---
+++ 101._Symmetric_Tree.cpp
@@ -0,0 +1,19 @@
+bool isSameTree(TreeNode* p, TreeNode* q){
+  if ( p == NULL && q == NULL )
+    return true;
+        
+  if ( p == NULL || q == NULL )
+    return false;
+
+  if( p->val != q->val)
+    return false;
+        
+   return isSameTree(p->left, q->right) && isSameTree(p->right, q->left);
+}
+
+bool isSymmetric(TreeNode* root) {
+  if(root == NULL)
+    return true;
+            
+  return isSameTree(root->left, root->right);
+}
