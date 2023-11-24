76cb4b2c9480c1bc6a3069f86f0b032639672a8b
zuzqqa <zuzannanowak04@wp.pl>
2023-11-25 00:02:32 +0100

    Create 219._Contains_Duplicate_IIv1.cpp

---
+++ 219._Contains_Duplicate_IIv1.cpp
@@ -0,0 +1,9 @@
+bool containsNearbyDuplicate(vector<int>& nums, int k) {
+  for( int i = 0; i < nums.size(); i++){
+    for( int j = i + 1; j < nums.size(); j++){
+      if(nums[i] == nums[j] && abs(i-j) <= k)
+        return true;
+    }
+  }
+  return false;
+}
