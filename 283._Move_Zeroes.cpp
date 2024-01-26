164d5987961d4e1ac913fedd9635815aa0c1695e
zuzqqa <zuzannanowak04@wp.pl>
2024-01-26 12:18:25 +0100

    Create 283._Move_Zeroes.cpp

---
+++ 283._Move_Zeroes.cpp
@@ -0,0 +1,10 @@
+void moveZeroes(vector<int>& nums) {
+        int nonZeroIndex = 0;
+
+        for (int i = 0; i < nums.size(); i++) {
+            if(nums[i] != 0) {
+                swap(nums[nonZeroIndex], nums[i]);
+                nonZeroIndex++;
+            }
+        }
+    }
